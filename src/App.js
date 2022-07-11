2e0020c3ede6673855aebcfa7c3495ffe7bad86b
Emanuel Gomila <gomilaemanuel@gmail.com>
2022-07-11 20:27:03 -0300

    vista

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import Propiedades from "./components/Propiedades";
 import React from "react";
 import Estado from "./components/Estado";
 import RenderizadoCondicional from "./components/RenderizadoCondicional";
+import RenderizadoElementos from "./components/RenderizadoElementos";
+import { EventosES7, EventosES6, MasSobreEventos } from "./components/Eventos";
+import Padre from "./components/ComunicacionComponentes";
 
 const App = () => {
   
   return (
     <div className="container bg-dark text-center text-light">
-      
+      <Padre />
+      <hr /><br />
+      <MasSobreEventos />
+      <hr /><br />
+      <EventosES7 />
+      <hr /><br />
+      <EventosES6 />
+      <hr /><br />
+      <RenderizadoElementos />
       <hr /><br />
       <RenderizadoCondicional />
       <hr /><br />
